f4141f4d1a40965ea0a6f25ea0a9e30c290130b7
Dragoș Moldovan-Grünfeld <dragos.mold@gmail.com>
2026-01-17 13:08:21 +0000

    tests

--- tests/testthat/test-digest.R
+++ tests/testthat/test-digest.R
@@ -1,5 +1,10 @@
 test_that("digest_coverage() works", {
-  cov <- readRDS(test_path("fixtures", "sample_coverage.RDS"))
+  cov <- readRDS(
+    test_path(
+      "fixtures",
+      "sample_coverage.RDS"
+    )
+  )
 
   expect_snapshot(
     digest_coverage(cov)
@@ -8,5 +13,12 @@ test_that("digest_coverage() works", {
   expect_s3_class(
     digest_coverage(cov),
     "tbl_df"
+  )
+})
+
+test_that("digest_coverage() complains", {
+  expect_snapshot(
+    error = TRUE,
+    digest_coverage(mtcars)
   )
 })
