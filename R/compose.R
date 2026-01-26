f80122b08db12da271240b0c2867ef94e441ddac
Dragoș Moldovan-Grünfeld <dragos.mold@gmail.com>
2026-01-26 20:25:44 +0000

    extract diff coverage

--- R/compose.R
+++ R/compose.R
@@ -82,8 +82,14 @@ compose_comment <- function(
     keep_all_files <- TRUE
   }
 
-  httr2::curl_help()
-
+  # TODO given that changed_files is kinda optional, make sure
+  # compose_coverage_details works
+  # FIXME changed_files is misleading a bit. I think the focus should be not so
+  # much on the files for which the content has changed, but rather on the files
+  # for which the coverage has changed.
+  #   * we can have PRs which do not touch files under R/ or src/ but rather
+  #   focus on adding more unit tests. These are currently note reported, but
+  #   they should.
   diff_md_table <- compose_coverage_details(
     head_coverage = head_coverage,
     base_coverage = base_coverage,
@@ -91,114 +97,13 @@ compose_comment <- function(
     keep_all_files = keep_all_files,
     align = align
   )
-  # browser()
+
   pr_details <- get_pr_details(
     repo = repo,
     pr_number = pr_number
   )
 
-  basehead <- glue::glue("{pr_details$base_name}...{pr_details$head_name}")
-
-  req_url <- glue::glue(
-    "https://api.github.com/repos/{repo}/compare/{basehead}"
-  )
-
-  # TODO maybe identify orphan tests?
-  # ! check
-
-  # we can use this to somehow get the lines of code added
-  reply <- glue::glue("GET {req_url}") |>
-    gh::gh()
-
-  line_details <- reply$files |>
-    purrr::keep(~ .x$filename %in% changed_files)
-
-  diff_text <- line_details[[1]]$patch
-  diff_lines <- diff_text |>
-    stringr::str_split("\n")
-
-  line_details |>
-    purrr::map(~ purrr::keep_at(.x, c("filename", "patch"))) |>
-    purrr::map(
-      ~ tibble::tibble(
-        file = .x$filename,
-        patch = .x$patch
-      )
-    ) |>
-    purrr::list_rbind() |>
-    dplyr::mutate(
-      patch = stringr::str_split(patch, "\n")
-    ) |>
-    tidyr::unnest(patch) |>
-    dplyr::group_by(file) |>
-    dplyr::mutate(
-      num = dplyr::row_number()
-    ) |>
-    dplyr::ungroup() |>
-    dplyr::select(
-      file,
-      num,
-      patch
-    )
-
-  # TODO see how this behaves with more complicated diffs - eg a tfrmt one
-  # TODO move this logic out -> its focus is to identify which lines in head
-  # have seen modifications in the PR. Then we can see what percentage of these
-  # are covered by tests
-  new_line_numbers_df <- tibble::tibble(
-    line = diff_lines[[1]]
-  ) |>
-    dplyr::mutate(
-      num = dplyr::row_number()
-    ) |>
-    dplyr::mutate(
-      hunk_header = stringr::str_detect(line, "@@"),
-      new_start = dplyr::if_else(
-        hunk_header,
-        stringr::str_extract(line, "\\+(\\d+)"),
-        NA_character_
-      ),
-      new_start = stringr::str_remove_all(
-        new_start,
-        stringr::fixed("+")
-      ),
-      new_start = as.numeric(new_start)
-    ) |>
-    dplyr::mutate(
-      include = dplyr::case_when(
-        hunk_header ~ 0,
-        stringr::str_starts(line, stringr::fixed("++")) ~ 0,
-        stringr::str_starts(line, stringr::fixed("-"), negate = TRUE) ~ 1,
-        .default = 0
-      ),
-      increment = dplyr::lag(include),
-      prep = dplyr::coalesce(new_start, increment),
-      new_line = cumsum(prep),
-      new_line = dplyr::if_else(
-        include == 0,
-        NA_real_,
-        new_line
-      )
-    )
-
-  b <- new_line_numbers_df |>
-    dplyr::select(num, line, new_line) |>
-    dplyr::mutate(
-      added = stringr::str_starts(
-        line,
-        stringr::fixed("+")
-      )
-    )
-
-  a <- covr:::to_report_data(head_coverage) |>
-    purrr::pluck("full") |>
-    purrr::list_rbind(names_to = "file")
-
-  hunk_header <- diff_lines[[1]][1]
-  # confirm by checking the hunk header contains @@
-  stringr::str_detect(hunk_header, "@@")
-  output_line_diff_start <-
-    total_head_coverage <- covr::percent_coverage(head_coverage)
+  total_head_coverage <- covr::percent_coverage(head_coverage)
   total_base_coverage <- covr::percent_coverage(base_coverage)
   delta_total_coverage <- round(total_head_coverage - total_base_coverage, 2)
 
@@ -207,6 +112,17 @@ compose_comment <- function(
   coverage_summary <- compose_coverage_summary(
     pr_details,
     delta_total_coverage
+  )
+
+  diff_line_coverage <- get_diff_line_coverage(
+    repo = repo,
+    pr_details = pr_details,
+    changed_files = changed_files,
+    head_coverage = head_coverage
+  )
+
+  diff_coverage_summary <- compose_diff_coverage_summary(
+    diff_line_coverage
   )
 
   # TODO update URL with the correct pkgdown one once there is one
@@ -224,10 +140,11 @@ compose_comment <- function(
     ![badge]({badge_url})
 
     {coverage_summary}
+    {diff_coverage_summary}
 
     <details>
 
-    <summary>Details on impacted files</summary>
+    <summary>Details on file coverage change</summary>
     <br/>
 
     {diff_md_table}
@@ -236,6 +153,14 @@ compose_comment <- function(
 
     <br/>
 
+    <details>
+
+    <summary>Details on line coverage change</summary>
+    <br/>
+
+    {diff_md_table}
+
+    </details>
 
     :recycle: Comment updated with the latest results.
 
@@ -350,5 +275,22 @@ compose_coverage_details <- function(
   diff_df_to_md(
     diff_df,
     align = align
+  )
+}
+
+compose_diff_coverage_summary <- function(diff_line_coverage) {
+  diff_coverage <- diff_line_coverage |>
+    dplyr::summarise(
+      total_lines_added = sum(.data$lines_added),
+      total_lines_covered = sum(.data$lines_covered)
+    )
+
+  percentage_line_coverage <- diff_coverage$total_lines_covered /
+    diff_coverage$total_lines_added
+
+  glue::glue(
+    "Diff coverage: {round(percentage_line_coverage, 2)}% \\
+    ({diff_coverage$total_lines_covered} out of \\
+    {diff_coverage$total_lines_added} added lines are covered by tests)."
   )
 }
