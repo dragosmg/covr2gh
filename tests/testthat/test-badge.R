fa2844275e18ad498ae21728ee78571195faa062
Dragoș Moldovan-Grünfeld <dragos.mold@gmail.com>
2026-01-28 21:14:48 +0000

    tests

--- tests/testthat/test-badge.R
+++ tests/testthat/test-badge.R
@@ -23,3 +23,43 @@ test_that("derive_badge_colour() with NULL and NA", {
     "#9f9f9f"
   )
 })
+
+test_that("generate_badge", {
+  expect_snapshot(
+    (generate_badge(5))
+  )
+
+  expect_snapshot(
+    (generate_badge(25))
+  )
+
+  expect_snapshot(
+    (generate_badge(55))
+  )
+
+  expect_snapshot(
+    (generate_badge(75))
+  )
+
+  expect_snapshot(
+    (generate_badge(95))
+  )
+})
+
+test_that("generate_badge with NA, NULL and out-of-bounds values", {
+  expect_snapshot(
+    (generate_badge(NA_real_))
+  )
+
+  expect_snapshot(
+    (generate_badge(NULL))
+  )
+
+  expect_snapshot(
+    (generate_badge(1302))
+  )
+
+  expect_snapshot(
+    (generate_badge(-5))
+  )
+})
