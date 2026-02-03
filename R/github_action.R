6850e60042616b8a0e6311185bcc1e1572bca4f8
Dragoș Moldovan-Grünfeld <dragos.mold@gmail.com>
2026-02-03 08:12:24 +0000

    docs for use_covr2gh_action()

--- R/github_action.R
+++ R/github_action.R
@@ -1,12 +1,55 @@
+#' Set up a covr2gh GitHub Action workflow
+#'
+#' Sets up a [GitHub Actions](https://github.com/features/actions) workflow that
+#' calculates and reports test coverage:
+#'   * uses [covr::package_coverage()].
+#'   * on a *pull request*:
+#'     * calculates test coverage for head.
+#'     * builds a badge (for use in the commit message).
+#'       * if the PR does not come from a fork (write privileges for the
+#'       automated GitHub Actions token), the badge is built with an internal
+#'       function ([generate_badge()]) and committed to the `covr2gh-storage`
+#'       branch.
+#'       * if the PR comes from a fork (the GitHub Actions token only has read
+#'       permissions), the badge is built with an external service (shields.io)
+#'       and is not committed to the storage branch.
+#'     * initialises or switches to the `covr2gh-storage` branch and saves a
+#'       copy of the badge (only if the PR does not come from a fork).
+#'     * switches to base branch and calculates test coverage.
+#'     * compares coverage for head and base and posts a comment with the main
+#'       findings.
+#'     * uploads base coverage and badge as artifacts.
+#'     * posts a workflow [summary](https://github.blog/news-insights/product-news/supercharging-github-actions-with-job-summaries/).
+#'   * on a *push to main*:
+#'     * calculates test coverage for head and creates a badge.
+#'     * switches to or creates the `covr2gh-storage` branch.
+#'     * commits badge (which is referenced in the README).
+#'     * uploads coverage as an artefact.
+#'
+#' `use_covr2gh_action()` wraps [usethis::use_github_action()].
+#'
+#' @param badge (logical) should a badge be added to README? Defaults to `TRUE`.
+#'
+#' @export
+#' @examples
+#' \dontrun{
+#' use_covr2gh_action()
+#' }
 use_covr2gh_action <- function(badge = TRUE) {
-    use_github_action(
-        url = "",
+    usethis::use_github_action(
+        url = "https://github.com/dragosmg/covr2gh/blob/main/.github/workflows/covr2gh.yaml",
+        # TODO add a link to the pkgdown article once finished
+        readme = NULL
     )
 
     if (isTRUE(badge)) {
         use_covr2gh_badge()
     }
+
+    invisible(TRUE)
 }
+
+
 use_covr2gh_badge <- function() {
     check_is_package()
 
@@ -17,16 +60,18 @@ use_covr2gh_badge <- function() {
         href = "",
         src = image_source
     )
+
+    invisible(TRUE)
 }
 
 is_package <- function(base_path = usethis::proj_get()) {
-    res <- tryCatch(
+    result <- tryCatch(
         rprojroot::find_package_root_file(
             path = base_path
         ),
         error = function(e) NULL
     )
-    !is.null(res)
+    !is.null(result)
 }
 
 check_is_package <- function(call = rlang::caller_env()) {
