bd0958df23180b57908cf695b2db0baf3ca8f1ad
Dragoș Moldovan-Grünfeld <dragos.mold@gmail.com>
2026-02-03 12:11:40 +0000

    use relative path to workflow

--- R/github_action.R
+++ R/github_action.R
@@ -55,10 +55,9 @@ use_covr2gh_action <- function(badge = TRUE) {
 #' Add a coverage README badge
 #'
 #' Inserts the markdown text for a coverage badge in the usethis block (between
-#' `<!-- badges: start -->` and `<!-- badges: end -->`). The URL for the badge
-#' SVG points to a storage branch in the repo(`covr2gh-storage`). The link out
-#' points to the covr2gh pkgdown website (not yet - TODO update once there is
-#' an article to point to).
+#' `<!-- badges: start -->` and `<!-- badges: end -->`). The `src` URL for the
+#' badge SVG points to a storage branch in the repo(`covr2gh-storage`). The link
+#' out points to the actions workflow page.
 #'
 #' @export
 #'
