c505c7263c6890f0bd7b3f2168bbe679b3ffe342
Dragoș Moldovan-Grünfeld <dragos.mold@gmail.com>
2026-02-05 23:37:06 +0000

    minor tweaks

--- R/line_coverage.R
+++ R/line_coverage.R
@@ -46,9 +46,10 @@ compose_line_coverage_summary <- function(diff_line_coverage, target = 80) {
             lines_covered = diff_coverage$total_lines_covered,
             lines_added = diff_coverage$total_lines_added
         ),
-        "{emoji} Diff coverage is {line_coverage}% ({lines_covered} out of \\
-        {lines_added} added lines are covered by tests). Minimum accepted is \\
-        `{target}%`."
+        "{emoji} Diff coverage is `{line_coverage}%` (`{lines_covered}` out \\
+        of `{lines_added}` added lines are covered by tests). It's good \\
+        practice to aim for at least `{target}%` (the base branch test \\
+        coverage)."
     )
 }
 
