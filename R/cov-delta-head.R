2a070d21c320cc8e4443da5e8879d49022f2b7a5
Dragoș Moldovan-Grünfeld <dragos.mold@gmail.com>
2026-04-30 14:25:18 +0100

    split cov-delta-coverage.r in 2: cov-delta-head.R and cov-delta-base.R

---
+++ R/cov-delta-head.R
@@ -0,0 +1,21 @@
+cov_delta_head_cov <- function(cov_delta, head_cov) {
+    check_cov_delta(cov_delta)
+    check_coverage(head_cov)
+
+    cov_delta$head_coverage <- head_cov
+    cov_delta
+}
+
+cov_delta_get_head_cov <- function(cov_delta) {
+    check_cov_delta(cov_delta)
+
+    head_cov <- cov_delta$head_coverage
+
+    perc_head_cov <- dplyr::if_else(
+        is.null(head_cov),
+        "empty",
+        paste0(round(covr::percent_coverage(head_cov), 1), "%")
+    )
+
+    perc_head_cov
+}
