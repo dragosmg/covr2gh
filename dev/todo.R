05901064eff1e8df0b404d09b1ab6965bd2843c2
Dragoș Moldovan-Grünfeld <dragos.mold@gmail.com>
2026-05-12 19:07:44 +0100

    todo

--- dev/todo.R
+++ dev/todo.R
@@ -7,18 +7,6 @@
 # * details is calculated
 # * body is calculated
 
-# TODO the workflow should not run when there are no changes to relevant files
-# TODO or the workflow runs, but the output reflects the state and there is no
-# need to compare head and base (basically, exit early)
-
-# TODO "this pr does not contain changes to files that would impact coverage"
-
-# TODO badge in gh-pages?
-# TODO covr object cached in gh-pages?
-# TODO cache the covr object for the most recent commit in main?
-
-# TODO change marker to <!-- covr2gh: do not delete/edit this line -->
-
 # * cov_delta is the input (created with `cov_delta()` and `new_cov_delta()`):
 #   * it needs
 #     * repo - set with `cov_delta_repo()`
@@ -37,17 +25,6 @@
 #   * pr_api_request -> perform -> pr_info (response)
 
 # * handle
-
-# * weird behaviour: old (running) checks pick-up the most recent commit and
-# report on it. the comment references a commit for which the checks have not
-# yet finished. I have no idea which version of the repo is being used
-# (probably the most recent one). this could potentially result in duplicated
-# runs. this is an issue for multigrain where runs take > 10 minutes.
-
-# * speak of changed (modified) lines not added lines. codecov calls them
-# "modified and coverable"
-
-# * line reference seems off (in multigrain it was 1 or 2 lines off)
 
 # we create a cov_delta object
 head_cov <- readRDS("dev/covr_head_pr_33_demo.rds")
