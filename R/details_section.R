3bd3bdc275d1a1d316c067dbb9634583daf6fd46
Dragoș Moldovan-Grünfeld <dragos.mold@gmail.com>
2026-05-18 20:42:04 +0100

    compose_details_section update

--- R/details_section.R
+++ R/details_section.R
@@ -1,8 +1,37 @@
 # brings together file coverage details and line coverage details
 compose_details_section <- function(
-    file_coverage_details,
-    line_coverage_details
+    file_cov_delta,
+    line_cov_delta,
+    call = rlang::caller_env()
 ) {
+    rlang::check_data_frame(
+        file_cov_delta,
+        allow_null = TRUE,
+        call = call
+    )
+    rlang::check_data_frame(
+        line_cov_delta,
+        allow_null = TRUE,
+        call = call
+    )
+
+    files <- setdiff(file_cov_delta$file_name, "Overall")
+
+    if (rlang::is_empty(files)) {
+        return(glue::as_glue(""))
+    }
+
+    file_coverage_details <- compose_file_coverage_details(
+        file_cov_delta
+    )
+
+    line_coverage_details <- compose_line_coverage_details(
+        line_cov_delta
+    )
+
+    # TODO probably not realistic / testable. for compose_file_coverage() to
+    # return "" it would mean file_cov_delta is NULL -> the
+    # rlang::is_empty(files) condition would have kicked in by now
     if (file_coverage_details == "" && line_coverage_details == "") {
         return(glue::as_glue(""))
     }
